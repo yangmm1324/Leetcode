2bde50d9efa79dcaf67a446e2f0742f5c82730fa
yangmm1324 <yangmm1324@gmail.com>
2021-09-14 20:42:19 -0700

    leetcode1452

---
+++ leetcode 1452 People Whose List of Favorite Companies Is Not a Subset of Another List.py
@@ -0,0 +1,20 @@
+class Solution:
+    def peopleIndexes(self, favoriteCompanies: List[List[str]]) -> List[int]:
+
+        words_index = collections.defaultdict(set)
+
+        for i, favorite in enumerate(favoriteCompanies):
+            for word in favorite:
+                words_index[word].add(i)
+
+        ans = []
+
+        for i, favorite in enumerate(favoriteCompanies):
+            candidate = set(words_index[favorite[0]])
+            for word in favorite[1:]:
+                candidate &= words_index[word]
+
+            if len(candidate) == 1:
+                ans.append(i)
+
+        return ans 
